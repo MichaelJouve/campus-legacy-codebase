621df4be70933029c8c1abfac5363d78eb67d4ac
MJO <mjouve@vega-systems.com>
2019-01-07 16:32:16 +0100

    refacto

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,4 +1,9 @@
 package com.gildedrose;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GildedRose {
     Item[] items;
@@ -6,70 +11,129 @@ public class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
+    private Logger logger = LoggerFactory.getLogger(GildedRose.class);
 
+    /**
+     * Update quality and reduce sellIn.
+     */
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")&& !items[i].name.equals("Conjured Mana Cake")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                    if (items[i].name.equals("Conjured Mana Cake")) {
-                        if (items[i].quality > 1) {
-                            items[i].quality = items[i].quality - 2;
-                        }
-                        if (items[i].quality < 2) {
-                            items[i].quality = 0;
-                        }
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        try {
+            logger.info("Start updateQuality");
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            for (int i = 0; i < items.length; i++) {
+                ItemAttributs itemAttributs = buildAttributs(items[i]);
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                increaseQuality(itemAttributs, i);
+                reduiceQuality(itemAttributs, i);
+                reduiceSellIn(itemAttributs, i);
             }
+        } catch (Exception error) {
+            logger.error("fail during the process", error, this.items);
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                if (items[i].name.equals("Conjured Mana Cake") && items[i].quality > 1) {
-                                    items[i].quality = items[i].quality - 2;
-                                } else {
-                                    items[i].quality = items[i].quality - 1;
-                                }
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+    public class ItemAttributs {
+        int reduiceSellIn =0;
+        int reduiceQuality = 0;
+        int increaseQuality = 0;
+
+        // true if quality null after the SellIn date
+        boolean atSellInZero = false;
+
+        // map<nb days, to increase x quality>
+        Map<Integer, Integer> dateQuality = new HashMap<>();
+    }
+
+    /**
+     * Use to build the specifics attributs of each products.
+     * Those attributs are used by the others functions to
+     * @param item
+     * @return
+     */
+    private ItemAttributs buildAttributs( Item item) {
+        ItemAttributs itemAttributs = new ItemAttributs();
+
+        itemAttributs.increaseQuality = 0;
+        itemAttributs.reduiceQuality = 1;
+        itemAttributs.reduiceSellIn = 1;
+        itemAttributs.atSellInZero = false;
+        itemAttributs.dateQuality.clear();
+
+        switch (item.name) {
+            case "Aged Brie":
+                itemAttributs.increaseQuality = 1;
+                itemAttributs.reduiceQuality = 0;
+                itemAttributs.reduiceSellIn = 1;
+                //itemAttributs.dateQuality.put(0,1);
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                itemAttributs.increaseQuality = 1;
+                itemAttributs.reduiceQuality = 0;
+                itemAttributs.reduiceSellIn = 0;
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                itemAttributs.increaseQuality = 1;
+                itemAttributs.reduiceQuality = 0;
+                itemAttributs.reduiceSellIn = 1;
+                itemAttributs.atSellInZero = true;
+                itemAttributs.dateQuality.put(10,1);
+                itemAttributs.dateQuality.put(3,1);
+
+                break;
+            case "Conjured Mana Cake":
+                itemAttributs.increaseQuality = 0;
+                itemAttributs.reduiceQuality = 2;
+                itemAttributs.reduiceSellIn = 1;
+                itemAttributs.dateQuality.put(-1,-2);
+                break;
+        }
+        return itemAttributs ;
+    }
+
+    private void reduiceQuality(ItemAttributs itemAttributs, int i){
+
+        this.items[i].quality = this.items[i].quality - itemAttributs.reduiceQuality;
+
+        // Set to zero the quality if it was negative
+        if (this.items[i].quality < 0) {
+            this.items[i].quality = 0;
+        }
+        // reduce at zero the quality if atSellInZero is true
+        if (itemAttributs.atSellInZero && this.items[i].sellIn <= -1) {
+            this.items[i].quality = 0;
+        }
+    }
+
+    private void increaseQuality(ItemAttributs itemAttributs, int i) {
+        this.items[i].quality = this.items[i].quality + itemAttributs.increaseQuality;
+
+        // increase at specific date before the end date (Map)
+        if (!itemAttributs.dateQuality.isEmpty()) {
+
+           for(Map.Entry<Integer, Integer> entry: itemAttributs.dateQuality.entrySet()){
+
+                if (this.items[i].sellIn <= entry.getKey()) {
+                    this.items[i].quality = this.items[i].quality + entry.getValue();
                 }
             }
         }
+
+        switch (this.items[i].name) {
+            case "Sulfuras, Hand of Ragnaros":
+                if (this.items[i].quality > 80) {
+                    this.items[i].quality = 80;
+                }
+                break;
+        }
+
+        if (this.items[i].quality > 50 && !this.items[i].name.contains("Sulfuras")) {
+            this.items[i].quality = 50;
+        }
+    }
+
+    private void reduiceSellIn(ItemAttributs intemAttributs, int i) {
+        this.items[i].sellIn = this.items[i].sellIn - intemAttributs.reduiceSellIn;
     }
 
     public Item[] getItems() {
